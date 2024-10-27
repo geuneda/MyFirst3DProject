16f3e9b5c63565ca54aed626e04efeb81af2d594
geuneda <whdrmsvjzl@naver.com>
2024-10-27 11:51:20 +0900

    fix: prevent structures from being collectible
    - 구조물이 아이템처럼 획득되지 않도록 수정

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -6,14 +6,21 @@ public class ItemObject : MonoBehaviour, IInteractable
 
     public string GetInteractPrompt()
     {
-        string str = $"{data.displayName}\n{data.description}";
+        string str = $"{data.displayName}\n\n{data.description}";
         return str;
     }
 
     public void OnInteract()
     {
+
+        if (data.type == ItemType.Structure)
+        {
+            return;
+        }
+
         CharacterManager.Instance.Player.itemData = data;
         CharacterManager.Instance.Player.addItem?.Invoke();
         Destroy(gameObject);
+        
     }
 }
