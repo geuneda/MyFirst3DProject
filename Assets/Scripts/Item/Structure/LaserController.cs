f942154ab83159b3dae527abedf9315eafe12eb0
geuneda <whdrmsvjzl@naver.com>
2024-10-29 10:52:43 +0900

    feat: add laser that detects and damages player
    - 플레이어를 감지하여 데미지를 주는 레이저 기능
    - 레이저와 플레이어 간 충돌 시 지속적으로 데미지를 입힘

---
+++ Assets/Scripts/Item/Structure/LaserController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public float segmentLength = 1f;
+    public float laserLength = 10f;
+    public LayerMask hitLayer;
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = Mathf.CeilToInt(laserLength / segmentLength) + 1;
+    }
+
+    private void Update()
+    {
+        ShootLaser();
+    }
+
+    private void ShootLaser()
+    {
+        Vector3 start = transform.position;
+        Vector3 direction = transform.forward;
+
+        for (int i = 0; i < lineRenderer.positionCount - 1; i++)
+        {
+            RaycastHit hit;
+            Vector3 segmentEnd = start + direction * segmentLength;
+
+            if (Physics.Raycast(start, direction, out hit, segmentLength, hitLayer))
+            {
+                lineRenderer.SetPosition(i, start);
+                lineRenderer.SetPosition(i + 1, hit.point);
+                CharacterManager.Instance.Player.condition.TakePhysicalDamage(1);
+            }
+            else
+            {
+                lineRenderer.SetPosition(i, start);
+                start = segmentEnd;
+            }
+        }
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, start);
+    }
+}
